b99c73d22279faa69469fe4e555da36007f2195e
AsadUrRehmanCs111 <55739556+AsadUrRehmanCs111@users.noreply.github.com>
2021-01-21 21:40:41 +0500

    ✨ Fuction added for responsiveness

--- 01.Responsive-NavBar/app.js
+++ 01.Responsive-NavBar/app.js
@@ -1,6 +1,6 @@
-const toggleBtn = document.getElementsByClassName('togglebtn')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleBtn = document.getElementsByClassName('togglebtn')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleBtn.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
 })
