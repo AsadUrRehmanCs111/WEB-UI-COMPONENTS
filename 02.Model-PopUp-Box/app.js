49401ea9b9dc6d12edbca258b4063e6bfd2f9997
AsadUrRehmanCs111 <55739556+AsadUrRehmanCs111@users.noreply.github.com>
2021-01-22 22:00:13 +0500

    ⚡️ Function added to hide and Show the Model

--- 02.Model-PopUp-Box/app.js
+++ 02.Model-PopUp-Box/app.js
@@ -1,4 +1,4 @@
-function modelPopUp() {
-    const popup = document.getElementById('model')
-    popup.classList.toggle('active');
+function popUp_model(){
+    const pop_up_model = document.getElementById('model');
+    pop_up_model.classList.toggle('active');
 }
